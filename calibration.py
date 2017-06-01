8a1bd0c620a5a902449adad308a42c456cf7aee2
Isharaka Gunasinghe <isharaka@yahoo.com>
2017-06-01 21:51:16 +1000

    Updated calbration function

--- calibration.py
+++ calibration.py
@@ -7,13 +7,14 @@ import pickle
 
 CALIBRATION_FILE = "calibration.p"
 
-def calibrate():
+# Calibrate camera and return calibration and distortion data
+def calibrate(debug=False):
     # Make a list of calibration images
     images = glob.glob('camera_cal/calibration*.jpg')
 
-    # prepare object points
-    nx = 9#TODO: enter the number of inside corners in x
-    ny = 6#TODO: enter the number of inside corners in y
+    # prepare object and image points
+    nx = 9
+    ny = 6
 
     objpoints = []
     imgpoints = []
@@ -41,11 +42,14 @@ def calibrate():
             # Draw and display the corners
             cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
 
-        #plt.imshow(img)
-        #plt.show()
+        if (debug==True):
+            plt.imshow(img)
+            plt.show()
 
+    # Calculate calibration and distortian matrices
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img.shape[0:2], None, None)
 
+    # Save calibration data
     dist_pickle = {}
     dist_pickle['mtx'] = mtx
     dist_pickle['dist'] = dist
@@ -53,6 +57,7 @@ def calibrate():
 
     return mtx, dist
 
+# Retrieve saved calibration data 
 def calibration_data():
     dist_pickle = pickle.load(open( CALIBRATION_FILE, "rb"))
     return dist_pickle['mtx'], dist_pickle['dist']
@@ -61,15 +66,18 @@ if __name__ == '__main__':
     mtx, dist = calibrate()
     #mtx, dist = calibration_data()
 
-    img = cv2.imread('camera_cal/calibration2.jpg')
+    img = cv2.imread('test_images/straight_lines1.jpg')
     undist = cv2.undistort(img, mtx, dist, None, mtx)
+
+    cv2.imwrite('output_images/calibration_in.jpg',img)
+    cv2.imwrite('output_images/calibration_out.jpg',undist)
 
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
     f.tight_layout()
-    ax1.imshow(img)
-    ax1.set_title('Original Image', fontsize=50)
-    ax2.imshow(undist)
-    ax2.set_title('Undistorted Image', fontsize=50)
+    ax1.imshow(np.asarray(img)[:,:,::-1].copy())
+    ax1.set_title('Original Image')
+    ax2.imshow(np.asarray(undist)[:,:,::-1].copy())
+    ax2.set_title('Undistorted Image')
     plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 
     plt.show()
